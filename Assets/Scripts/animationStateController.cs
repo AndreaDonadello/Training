9078ae5cbfa4e708ba8adb4bf42e22cb37dfe8a4
AndreaDonadello <infoandreadonadello@gmail.com>
2021-01-21 12:42:35 +0100

    Initial commit

---
+++ Assets/Scripts/animationStateController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+
+
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        bool isrunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+       
+
+        //if player presses w key
+        if (!isWalking && forwardPressed)
+        {
+            // then set isWalking boolean to be true
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        // if player not pressing w key
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        // if player is walking and not running and presses left shift
+        if (!isrunning && (forwardPressed && runPressed))
+        {
+            // then set the isRunning boolean to be true
+            animator.SetBool(isRunningHash, true);
+        }
+
+        // if player is running and stops running or stops walking
+        if (isrunning && (!forwardPressed || !runPressed))
+        {
+            // then set the isRunning boolean to be false
+            animator.SetBool(isRunningHash, false);
+        }
+
+       
+    }
+       
+        
+        
+        
+        
+        
+     
+
+}
